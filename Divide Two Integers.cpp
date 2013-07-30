e1631f4e2a182cd48b8f28ec1c8cc2dc38dc0514
calmdan <duanlei.kq@gmail.com>
2013-07-30 21:04:10 +0800

    Create Divide Two Integers.cpp

---
+++ Divide Two Integers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        long long a = abs((double)dividend);;
+        long long b = abs((double)divisor);
+
+        long long ret = 0;
+        while (a >= b) {
+            long long c = b;
+            for (int i = 0; a >= c; ++i, c <<= 1) {
+                a -= c;
+                ret += 1 << i;
+            }
+        }
+
+        return ((dividend^divisor)>>31) ? (-ret) : (ret);
+    }
+};
