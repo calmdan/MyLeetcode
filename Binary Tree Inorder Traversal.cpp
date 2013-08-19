952647c38b100640e07b8c4728789ee369bd170f
calmdan <duanlei.kq@gmail.com>
2013-08-19 16:44:15 +0800

    Create Binary Tree Inorder Traversal.cpp

---
+++ Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<int> res;
+        inorder(res,root);
+        return res;        
+    }
+    void inorder(vector<int> &res,TreeNode *root)
+    {
+        if(root==NULL) return;
+        inorder(res,root->left);
+        res.push_back(root->val);
+        inorder(res,root->right);
+    }
+};
