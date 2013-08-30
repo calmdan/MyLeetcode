ec6ecf2fdeaaaa5309949a3f48d9b4862ff91734
calmdan <duanlei.kq@gmail.com>
2013-08-30 10:36:39 +0800

    Update Reverse Integer.cpp

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -1,3 +1,7 @@
+/*If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
+Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
+Throw an exception? Good, but what if throwing an exception is not an option? You would then have to re-design the function (ie, add an extra parameter).
+*/
 class Solution {
 public:
         int reverse(int x) {
@@ -15,7 +19,7 @@ public:
             x = x/10;
         }
     
-        if(result<0) return -1;
+        if(result<0) return -1;//判断溢出
     
         if(isNeg)
             result *=-1;
