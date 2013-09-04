dcaf96c42acdc785a01ff5f3d94848bfb9b29f6f
calmdan <duanlei.kq@gmail.com>
2013-09-04 16:43:21 +0800

    Update Next Permutation.cpp

--- Next Permutation.cpp
+++ Next Permutation.cpp
@@ -3,6 +3,38 @@ public:
     void nextPermutation(vector<int> &num) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
+        int n = num.size();
+        if(n==1) return;
+        bool flag = false;
+        int i =n-2;
+        for(;i>=0;i--)
+        {
+            if(num[i]<num[i+1])
+            {
+                flag = true;
+                break;
+            }
+        }
+        int j=i+1;
+        for(;j<n;j++)
+        {
+            if(num[j]<=num[i]) break;
+        }
+        j--;
+        if(!flag) 
+        {
+            reverse(num.begin(),num.end());
+            return;
+        }
+        swap(num[i],num[j]);
+        reverse(num.begin()+i+1,num.end());
+    }
+};
+class Solution {
+public:
+    void nextPermutation(vector<int> &num) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
         auto it = num.end()-1;
         while(it!=num.begin() && *(it-1)>=*it) it--;//find *(it-1) < *it
         if(it ==num.begin())
