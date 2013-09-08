3b677970030c28fe756198604909090326473c7d
calmdan <duanlei.kq@gmail.com>
2013-09-08 18:20:30 +0800

    Update Populating Next Right Pointers in Each Node II.cpp

--- Populating Next Right Pointers in Each Node II.cpp
+++ Populating Next Right Pointers in Each Node II.cpp
@@ -36,34 +36,34 @@ public:
 //与上一题类似，唯一的不同是每次要先找到一个第一个有效的next链接节点，并且递归的时候要先处理右子树，再处理左子树。
 
 
-[Code]
-1:    void connect(TreeLinkNode *root) {  
-2:      // Start typing your C/C++ solution below  
-3:      // DO NOT write int main() function  
-4:      if(root== NULL) return;  
-5:      TreeLinkNode* p = root->next;  
-6:      while(p!=NULL)  
-7:      {  
-8:         if(p->left!=NULL)  
-9:         {  
-10:            p = p->left;  
-11:            break;  
-12:        }  
-13:        if(p->right!=NULL)  
-14:        {  
-15:           p = p->right;  
-16:           break;  
-17:        }  
-18:        p = p->next;  
-19:      }  
-20:      if(root->right!= NULL)  
-21:      {        
-22:        root->right->next = p;  
-23:      }  
-24:      if(root->left !=NULL)  
-25:      {        
-26:        root->left->next = root->right? root->right:p;        
-27:      }  
-28:      connect(root->right);  
-29:      connect(root->left);      
-30:    }  
+
+    void connect(TreeLinkNode *root) {  
+      // Start typing your C/C++ solution below  
+      // DO NOT write int main() function  
+      if(root== NULL) return;  
+      TreeLinkNode* p = root->next;  
+      while(p!=NULL)  
+      {  
+         if(p->left!=NULL)  
+         {  
+            p = p->left;  
+            break;  
+        }  
+        if(p->right!=NULL)  
+        {  
+           p = p->right;  
+           break;  
+        }  
+        p = p->next;  
+      }  
+      if(root->right!= NULL)  
+      {        
+        root->right->next = p;  
+      }  
+      if(root->left !=NULL)  
+      {        
+        root->left->next = root->right? root->right:p;        
+      }  
+      connect(root->right);  //!!!!
+      connect(root->left);      
+    }  
