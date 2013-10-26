2df85ab0e37075567e1f453f250626aadd627007
calmdan <duanlei.kq@gmail.com>
2013-10-27 00:29:43 +0800

    Update Jump Game.cpp

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool canJump(int A[], int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if(n==0 || n==1) return true;
-        int reach=0;
-        int i=0;
-        while(i<n-1 && i<=reach)
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int i=0,reach =0;
+        while(i<=reach)
         {
-            reach = max(reach,i+A[i]);
+            reach = max(reach, i+A[i]);
             if(reach>=n-1) return true;
             i++;
         }
